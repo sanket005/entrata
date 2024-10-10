0328f61c81c2a027ee65bf1f132bec15d96a409f
sanket005 <110851111+sanket005@users.noreply.github.com>
2024-10-10 10:27:14 -0700

    Update UsernameAndPasswordVisibilityTest.java

--- src/test/java/Entrata_Test_Cases/UsernameAndPasswordVisibilityTest.java
+++ src/test/java/Entrata_Test_Cases/UsernameAndPasswordVisibilityTest.java
@@ -1,6 +1,8 @@
 package Entrata_Test_Cases;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
